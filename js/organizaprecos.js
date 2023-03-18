0d4604c7dd0cbd5f8dbf746bfcea4ff1ed0a3c0c
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-17 22:50:15 -0300

    Delete organizaprecos.js

--- js/organizaprecos.js
+++
@@ -1,9 +0,0 @@
-let btnOrdenarPreco = document.getElementById('btnOrdenarPorPreco');
-btnOrdenarPreco.addEventListener('click', ordernarLivrosPreco)
-
-function ordernarLivrosPreco (){
-
-    let livrosdescontados = aplicarDesconto(resconvert)
-    let livrosOrdenados = livrosdescontados.sort((a,b) => a.preco - b.preco);
-    exibirLivros(livrosOrdenados);
-}
