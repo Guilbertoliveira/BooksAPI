92afc55ceab2c8ad6d69e0b735bb2b03af63ad2d
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-17 21:41:31 -0300

    Filters javascript code update

--- js/exibicao.js
+++ js/exibicao.js
@@ -2,9 +2,10 @@ const divpai = document.getElementById('livros');
 
 function exibirLivros (lista) {
     lista.forEach(element => {
+      let disponibilidade = verificarDisponibilidade(element);
         divpai.innerHTML += 
         `<div class="livro">
-        <img class="livro__imagens" src=${element.imagem} alt=${element.alt} />
+        <img class="${disponibilidade}" src=${element.imagem} alt=${element.alt} />
         <h2 class="livro__titulo">
           ${element.titulo}
         </h2>
@@ -15,4 +16,13 @@ function exibirLivros (lista) {
         </div>
       </div>`
     });
+}
+
+
+function verificarDisponibilidade(livro) {
+  if (livro.quantidade > 0) {
+    return 'livro__imagens';
+  } else {
+    return 'livros__imagens indisponivel';
+  }
 }
---
+++ js/livrosdisponivel.js
@@ -0,0 +1,14 @@
+const btnDisponivel = document.getElementById('btnLivrosDisponiveis');
+
+btnDisponivel.addEventListener('click', ()=> {
+    console.table(resconvert)
+    let livrosDisponivel = resconvert.filter((element)=>{
+        return element.quantidade > 0;
+    })
+
+    console.table()
+    exibirLivros(aplicarDesconto(livrosDisponivel));
+    
+       
+    } ) 
+    
